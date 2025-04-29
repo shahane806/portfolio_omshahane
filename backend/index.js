f44c0264b3de6bc136eaedf93f11762ae1ab5a67
omshahane788 <omshahane788@gmail.com>
2025-04-29 14:42:17 +0530

    commit backend index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,20 @@
 const dotenv = require("dotenv");
 const cors = require('cors');
 const express = require("express");
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(
+    express.urlencoded({
+      extended: false,
+      limit: '100kb'
+    })
+);
+  
+const PORT = 3568 || process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`App is running on port ${PORT}`);
+})
